01dc66b0282e667ce59d9d850e0b333888f62f31
fire.phoenix.dev@gmail.com <fire.phoenix.dev@gmail.com>
2026-05-29 19:43:38 +0200

    modified:   lua/autorun/ktso_models.lua

--- lua/autorun/ktso_models.lua
+++ lua/autorun/ktso_models.lua
@@ -2,7 +2,7 @@
 -- написано лапками prizmaa. без вайбкода. через слёзы и хэнджоб.
 
 if CLIENT then
-    timer.Simple(5, function() -- функция для 717 эЛВэЗэ
+    timer.Simple(1, function() -- функция для 717 эЛВэЗэ
         local lvz_head = {"gmod_subway_81-717_lvz", "gmod_subway_81-717_lvz_custom"}
         for _, class in pairs(lvz_head) do
             local ENT = scripted_ents.GetStored(class)
@@ -37,18 +37,17 @@ if CLIENT then
             if ENT and ENT.t then
                 ENT.t.ClientProps = ENT.t.ClientProps or {}
 
-            ENT.t.ClientProps["begstroka_tabloR"] = {
+            ENT.t.ClientProps["begstroka_tabloR"] = { --табло у манометров
                     model = "models/metrostroi_train/81-5402/dev4you_begstroka.mdl",
-                    pos = Vector(-448.65,0,65.7),
+                    pos = Vector(-449.85,0,65.7),
                     ang = Angle(180,0,0),
                     nohide = true,
                 }
-            ENT.t.ClientProps["begstroka_tabloF"] = {
+            ENT.t.ClientProps["begstroka_tabloF"] = { --табло у... аа.. с другой стороны короче
                     model = "models/metrostroi_train/81-5402/dev4you_begstroka.mdl",
-                    pos = Vector(435.58,0,59),
+                    pos = Vector(449.15,0,57),
                     ang = Angle(0,0,0),
-                    scale=1,
-                    hide=1.5,
+                    nohide = true,
                 }
             end
         end
@@ -62,6 +61,69 @@ if CLIENT then
             ENT.t.ClientProps["kts"] = {
                     model = "models/metrostroi_train/81-5402/kts_5p.mdl",
                     pos = Vector(10,0,0),
+                    ang = Angle(0,0,0),
+                    nohide = true,
+                }
+            ENT.t.ClientProps["begstroka_tabloR"] = {
+                    model = "models/metrostroi_train/81-5402/dev4you_begstroka.mdl",
+                    pos = Vector(-462,0,57),
+                    ang = Angle(180,0,0),
+                    nohide = true,
+                }
+            end
+        end
+        local lvz_sabl_prom = {"gmod_subway_81-541_8"}
+        for _, class in pairs(lvz_sabl_prom) do
+            local ENT = scripted_ents.GetStored(class)
+
+            if ENT and ENT.t then
+                ENT.t.ClientProps = ENT.t.ClientProps or {}
+
+            ENT.t.ClientProps["begstroka_tabloR"] = { --табло у манометров
+                    model = "models/metrostroi_train/81-5402/dev4you_begstroka.mdl",
+                    pos = Vector(432.19,0,57),
+                    ang = Angle(0,0,0),
+                    nohide = true,
+                }
+            ENT.t.ClientProps["begstroka_tabloF"] = { --ай факд тхере
+                    model = "models/metrostroi_train/81-5402/dev4you_begstroka.mdl",
+                    pos = Vector(-449.85,0,65.7),
+                    ang = Angle(180,0,0),
+                    nohide = true,
+                }
+            end
+        end
+        local dottwo_head = {"gmod_subway_81-540_2_lvz"}
+        for _, class in pairs(dottwo_head) do
+            local ENT = scripted_ents.GetStored(class)
+
+            if ENT and ENT.t then
+                ENT.t.ClientProps = ENT.t.ClientProps or {}
+
+                ENT.t.ClientProps["begstroka_tabloR"] = {
+                    model = "models/metrostroi_train/81-5402/dev4you_begstroka.mdl",
+                    pos = Vector(-462,0,57),
+                    ang = Angle(180,0,0),
+                    nohide = true,
+                }
+            ENT.t.ClientProps["begstroka_tabloF"] = {
+                    model = "models/metrostroi_train/81-5402/dev4you_begstroka.mdl",
+                    pos = Vector(374.7,0,63),
+                    ang = Angle(0,0,0),
+                    nohide = true,
+                }
+            end
+        end
+        local lvz_head = {"gmod_subway_81-540"}
+        for _, class in pairs(lvz_head) do
+            local ENT = scripted_ents.GetStored(class)
+
+            if ENT and ENT.t then
+                ENT.t.ClientProps = ENT.t.ClientProps or {}
+
+            ENT.t.ClientProps["kts"] = {
+                    model = "models/metrostroi_train/81-5402/kts_5p.mdl",
+                    pos = Vector(0,0,0),
                     ang = Angle(0,0,0),
                     nohide = true,
                 }
